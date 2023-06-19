6538a022c213dd1a961e175e205b6b36f117e348
Bharti Patel <patelshree40@gmail.com>
2023-06-19 09:54:57 +0530

    basic funtionalities added

--- index.js
+++ index.js
@@ -1,17 +1,76 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-
-let pass1El = document.getElementById("pass1");
-let pass2El = document.getElementById("pass2");
-let inputEl = document.getElementById("len");
-let m = inputEl.value;
-let n = characters.length;
 function generate(){
-for(let i=0; i<8; i++){
-    let rand1 = Math.floor(Math.random()*n);
-    let rand2 = Math.floor(Math.random()*n);
-    pass1El.textContent += characters[rand1];
-    pass2El.textContent += characters[rand2];
+    let password = "";
+    let uppercaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lowercaseSet = "abcdefghijklmnopqrstuvwxyz";
+    let numSet = "1234567890";
+    let symbolSet = "!@#$%^&*()_+-=`~{}|][;':,./<>?";
 
- }
+    let uppercase = document.getElementById("uppercase").checked;
+    let lowercase = document.getElementById("lowercase").checked;
+    let symbols = document.getElementById("symbols").checked;
+    let numbers = document.getElementById("numbers").checked;
+    let length = document.getElementById("len").value;
+    let exclude = document.getElementById("similar").checked;
+
+    let charset = lowercaseSet;
+
+    if(!exclude){
+        lowercaseSet += "ilo";
+        uppercaseSet += "IO";
+        numSet += "01";
+    }
+
+    if(uppercase){
+      charset += uppercaseSet;
+    }
+
+    if(lowercase){
+        charset += lowercaseSet;
+    }
+
+    if(numbers){
+        charset += numSet;
+    }
+
+    if(symbols) {
+        charset += symbolSet;
+    }
+    
+    
+    for(let i=0; i<length; i++){
+    
+       password += charset[Math.floor(Math.random()*charset.length)];
+     }
+  document.getElementById("password").value = password;
+
+ let mnemonic = generateMnemonic(password);
+ 
+ document.getElementById("mnemonic").textContent = mnemonic;
 }
+
+ 
+
+function generateMnemonic(password) {
+    let words = {
+      "a": ["apple", "ant", "alligator"],
+      "b": ["banana", "bat", "book"],
+      "c": ["cherry", "cat", "car"],
+    };
+
+    let mnemonic = "";
+
+    for(let i=0; i<password.length; i++){
+        let char = password[i].toLowerCase();
+         if(char in words){
+            let word = words[char][Math.floor(Math.random() * words[char].length)];
+            mnemonic += word + " ";
+            }
+            else {
+                mnemonic += password[i] + " ";
+            }
+         }
+
+         return mnemonic.trim();
+        
+    }
+    
