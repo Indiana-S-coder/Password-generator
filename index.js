63d76b966f3bb777ca4b757062c74187c9159d79
Bharti Patel <patelshree40@gmail.com>
2023-07-19 07:53:23 +0530

    styling done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function generate(){
+function generate() {
     let password = "";
     let uppercaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let lowercaseSet = "abcdefghijklmnopqrstuvwxyz";
@@ -12,65 +12,87 @@ function generate(){
     let length = document.getElementById("len").value;
     let exclude = document.getElementById("similar").checked;
 
-    let charset = lowercaseSet;
+    // Use a separate variable for charset
+    let charset = "";
 
-    if(!exclude){
-        lowercaseSet += "ilo";
+    if (!exclude) {
+        // Reset lowercaseSet to its original value before exclusion
+        lowercaseSet = "abcdefghijklmnopqrstuvwxyz";
         uppercaseSet += "IO";
         numSet += "01";
     }
 
-    if(uppercase){
-      charset += uppercaseSet;
+    if (uppercase) {
+        charset += uppercaseSet;
     }
 
-    if(lowercase){
+    if (lowercase) {
         charset += lowercaseSet;
     }
 
-    if(numbers){
+    if (numbers) {
         charset += numSet;
     }
 
-    if(symbols) {
+    if (symbols) {
         charset += symbolSet;
     }
-    
-    
-    for(let i=0; i<length; i++){
-    
-       password += charset[Math.floor(Math.random()*charset.length)];
-     }
-  document.getElementById("password").value = password;
 
- let mnemonic = generateMnemonic(password);
- 
- document.getElementById("mnemonic").textContent = mnemonic;
+    for (let i = 0; i < length; i++) {
+        password += charset[Math.floor(Math.random() * charset.length)];
+    }
+    document.getElementById("password").value = password;
+    
+    let strengthScore = calculatePasswordStrength(password);
+    let strengthText = getStrengthText(strengthScore);
+    // let mnemonic = generateMnemonic(password);
+
+    // document.getElementById("mnemonic").textContent = mnemonic;
+    document.getElementById("strength").textContent = `Password Strength: ${strengthText}`;
 }
 
- 
+function calculatePasswordStrength(password) {
+    const result = zxcvbn(password);
+    return result.score;
+}
 
-function generateMnemonic(password) {
-    let words = {
-      "a": ["apple", "ant", "alligator"],
-      "b": ["banana", "bat", "book"],
-      "c": ["cherry", "cat", "car"],
-    };
-
-    let mnemonic = "";
-
-    for(let i=0; i<password.length; i++){
-        let char = password[i].toLowerCase();
-         if(char in words){
-            let word = words[char][Math.floor(Math.random() * words[char].length)];
-            mnemonic += word + " ";
-            }
-            else {
-                mnemonic += password[i] + " ";
-            }
-         }
-
-         return mnemonic.trim();
-        
+function getStrengthText(score) {
+    switch (score) {
+        case 0:
+            return "Very Weak";
+        case 1:
+            return "Weak";
+        case 2:
+            return "Fair";
+        case 3:
+            return "Strong";
+        case 4:
+            return "Very Strong";
+        default:
+            return "Unknown";
     }
+}
+
+// function generateMnemonic(password) {
+//     let words = {
+//         "a": ["apple", "ant", "alligator"],
+//         "b": ["banana", "bat", "book"],
+//         "c": ["cherry", "cat", "car"],
+//     };
+
+//     let mnemonic = "";
+
+//     for (let i = 0; i < password.length; i++) {
+//         let char = password[i].toLowerCase();
+//         if (char in words) {
+//             let word = words[char][Math.floor(Math.random() * words[char].length)];
+//             mnemonic += word + " ";
+//         } else {
+//             mnemonic += password[i] + " ";
+//         }
+//     }
+
+//     return mnemonic.trim();
+// }
+
     
